02b3c0acdaafdb0266c123bfc4ef2585fb8f4278
federico.lucca <federico.lucca@0fd5999a-94d0-4028-93f2-09691c278cb2>
2013-05-02 15:33:15 +0000

    - change name project

---
+++ src/main/java/eu/trentorise/smartcampus/coreservice/model/AppAccount.java
@@ -0,0 +1,53 @@
+package eu.trentorise.smartcampus.coreservice.model;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class AppAccount {
+	private String id;
+	private String appName;
+
+	@XmlElementWrapper
+	@XmlElement(name = "configuration")
+	private List<Configuration> configurations;
+	
+	@XmlElementWrapper
+	@XmlElement(name = "permission")
+	private List<Permission> permission;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getAppName() {
+		return appName;
+	}
+
+	public void setAppName(String appName) {
+		this.appName = appName;
+	}
+
+	public List<Configuration> getConfigurations() {
+		return configurations;
+	}
+
+	public void setConfigurations(List<Configuration> configurations) {
+		this.configurations = configurations;
+	}
+
+	
+	
+
+
+}
