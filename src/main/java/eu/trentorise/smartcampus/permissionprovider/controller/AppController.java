77533bf2a68b7c4fbbc7753c30eb5bf713a1c9ea
raman <raman@fbk.eu>
2013-05-21 14:18:39 +0200

    OAuth integrated; user data persistence added.

--- src/main/java/eu/trentorise/smartcampus/permissionprovider/controller/AppController.java
+++ src/main/java/eu/trentorise/smartcampus/permissionprovider/controller/AppController.java
@@ -1,3 +1,18 @@
+/**
+ *    Copyright 2012-2013 Trento RISE
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package eu.trentorise.smartcampus.permissionprovider.controller;
 
 
@@ -7,25 +22,18 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 import eu.trentorise.smartcampus.permissionprovider.exceptions.AlreadyExistException;
 import eu.trentorise.smartcampus.permissionprovider.exceptions.SmartCampusException;
 import eu.trentorise.smartcampus.permissionprovider.model.App;
 import eu.trentorise.smartcampus.permissionprovider.repository.AppRepository;
-import eu.trentorise.smartcampus.presentation.common.exception.NotFoundException;
 
 
 @Controller
@@ -49,7 +57,7 @@ public class AppController {
 	public @ResponseBody
 	App update(HttpServletRequest request,
 			@RequestBody App app, @PathVariable String appName)
-			throws SmartCampusException, NotFoundException {
+			throws SmartCampusException {
 		return appRepository.save(app);
 	}
 
--- src/main/java/eu/trentorise/smartcampus/permissionprovider/model/User.java
+++ src/main/java/eu/trentorise/smartcampus/permissionprovider/model/User.java
@@ -1,14 +1,77 @@
+/**
+ *    Copyright 2012-2013 Trento RISE
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package eu.trentorise.smartcampus.permissionprovider.model;
 
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 
 @Entity
-public class User {
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1067996326671906278L;
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue
 	private Long id;
+
+	@OneToMany(fetch = FetchType.LAZY, cascade = { CascadeType.PERSIST,
+			CascadeType.REMOVE, CascadeType.MERGE })
+	@JoinColumn(name = "USER_ID", nullable=false)
+	private Set<Attribute> attributeEntities;
+
+	@Column(name = "SOCIAL_ID")
+	private Long socialId;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Set<Attribute> getAttributeEntities() {
+		return attributeEntities;
+	}
+
+	public void setAttributeEntities(Set<Attribute> attributeEntities) {
+		this.attributeEntities = attributeEntities;
+	}
+
+	@Override
+	public String toString() {
+		return "UserEntity{id=" + id + ",socialId=" + socialId + "}";
+	}
+
+	public Long getSocialId() {
+		return socialId;
+	}
+
+	public void setSocialId(Long socialId) {
+		this.socialId = socialId;
+	}
 
 }
