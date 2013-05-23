ad12c9d4116aa696c6ad178b5e41e9e0bc46a3a4
raman <raman@fbk.eu>
2013-05-23 13:02:04 +0200

    Custom confirmation page, basic version

---
+++ src/main/java/eu/trentorise/smartcampus/permissionprovider/oauth/AccessConfirmationController.java
@@ -0,0 +1,59 @@
+/**
+ *    Copyright 2012-2013 Trento RISE
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package eu.trentorise.smartcampus.permissionprovider.oauth;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.provider.AuthorizationRequest;
+import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Controller for retrieving the model for and displaying the confirmation page for access to a protected resource.
+ * 
+ */
+@Controller
+@SessionAttributes("authorizationRequest")
+public class AccessConfirmationController {
+
+	@Autowired
+	private ClientDetailsService clientDetailsService;
+
+	@RequestMapping("/oauth/confirm_access")
+	public ModelAndView getAccessConfirmation(Map<String, Object> model) throws Exception {
+		AuthorizationRequest clientAuth = (AuthorizationRequest) model.remove("authorizationRequest");
+		ClientDetails client = clientDetailsService.loadClientByClientId(clientAuth.getClientId());
+		model.put("auth_request", clientAuth);
+		model.put("client", client);
+		return new ModelAndView("access_confirmation", model);
+	}
+
+	@RequestMapping("/oauth/error")
+	public String handleError(Map<String,Object> model) throws Exception {
+		model.put("message", "There was a problem with the OAuth2 protocol");
+		return "oauth_error";
+	}
+
+	@Autowired
+	public void setClientDetailsService(ClientDetailsService clientDetailsService) {
+		this.clientDetailsService = clientDetailsService;
+	}
+}
