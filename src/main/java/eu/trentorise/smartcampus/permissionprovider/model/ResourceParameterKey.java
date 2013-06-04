6547eb04ab325843eee311d57b6a604e96cf4d63
raman <raman@fbk.eu>
2013-06-04 10:49:19 +0200

    equals/hashcode

--- src/main/java/eu/trentorise/smartcampus/permissionprovider/model/ResourceParameterKey.java
+++ src/main/java/eu/trentorise/smartcampus/permissionprovider/model/ResourceParameterKey.java
@@ -28,4 +28,36 @@ public class ResourceParameterKey implements Serializable {
 
 	public String resourceId;
 	public String value;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((resourceId == null) ? 0 : resourceId.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ResourceParameterKey other = (ResourceParameterKey) obj;
+		if (resourceId == null) {
+			if (other.resourceId != null)
+				return false;
+		} else if (!resourceId.equals(other.resourceId))
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
+	
 }
