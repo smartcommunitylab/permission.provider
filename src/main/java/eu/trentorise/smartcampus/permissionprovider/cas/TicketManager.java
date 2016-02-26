771d0b4c2f917ee507bf9ae861966660f751dd64
Роман <kazhamiakin@gmail.com>
2016-02-26 11:39:17 +0100

    ticket log

--- src/main/java/eu/trentorise/smartcampus/permissionprovider/cas/TicketManager.java
+++ src/main/java/eu/trentorise/smartcampus/permissionprovider/cas/TicketManager.java
@@ -76,7 +76,7 @@ public class TicketManager {
 	public Ticket checkTicket(String service, String ticket) throws CASException{
 		Ticket ticketObj = ticketKeyStorage.get(ticket);
 		if (ticketObj == null || ticketObj.isExpired()) throw new CASException(ERROR_CODE.INVALID_TICKET, "Ticket does not exists or is expired."); 
-		if (!service.equals(ticketObj.service)) throw new CASException(ERROR_CODE.INVALID_SERVICE, "Service does not match ticket: service = "+service);
+		if (!service.equals(ticketObj.service)) throw new CASException(ERROR_CODE.INVALID_SERVICE, "Service does not match ticket: service = "+service+", expected = "+ticketObj.getService());
 		ticketStorage.remove(new TicketKey(ticketObj.id, service));
 		ticketKeyStorage.remove(ticket);
 		return ticketObj;
